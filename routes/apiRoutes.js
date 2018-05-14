06bcf573dc00f4faad8333889f73e0a7b6e26e01
Mohammed Hameed <mohammed.hameed@email.com>
2018-05-13 20:49:05 -0500

    built mock teacher info to check route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,4 +4,16 @@ module.exports = function(app){
     app.get("/api", function(req, res){
         res.send ("testing /app route")
     })
+
+    //teacher login route
+    app.get("/teacherLogin", function(req, res){
+        console.log(req.query)
+        let username = "keely",
+            password = "12345",
+            userID = 1;
+
+            if (req.query.username === username && req.query.password === password){
+                res.send({id: userID})
+            }
+    })
 }
