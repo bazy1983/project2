61af695c907ef77f9e7b5935bc0a7b706d8a8ac7
Mohammed Hameed <mohammed.hameed@email.com>
2018-05-14 19:34:27 -0500

    removed force drop from sequelize sync

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ require("./routes/htmlRoutes")(app);
 
 
 
-db.sequelize.sync({force : true})
+db.sequelize.sync()
 // .then(function() {
 //     app.listen(PORT, function() {
 //       console.log("App listening on PORT " + PORT);
