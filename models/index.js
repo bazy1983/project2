99b578c744dff1abfda7cc58b8d30e8844d6e537
Carolina RC <caro.rmz315@gmail.com>
2018-05-18 23:23:17 -0500

    moved questions below submit button

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config1.json')[env];
+// CHANGED TO CONFIG.JSON
+var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
